cc8b9bcaf6240bc4c2b535fd95a8a93fc83fd2ff
rastringer <ra1stringer@gmail.com>
2023-12-05 14:46:04 +0000

    day 1

---
+++ day_1_python/p_1.py
@@ -0,0 +1,17 @@
+# Part One
+
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+total = 0
+
+for l in lines:
+    digits = []
+    for c in l:
+        if c.isdigit():
+            digits.append(c)
+    concat = int(digits[0]+digits[-1])
+    total += concat
+
+
+print(total)      
---
+++ day_1_python/p_2.py
@@ -0,0 +1,33 @@
+# Part Two
+with open("input.txt") as f:
+    lines = []
+    for line in f:
+        cleaned_line = line.rstrip("\n") 
+        lines.append(cleaned_line)
+
+digit_dict = {"one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+        }
+
+total = 0
+i = 0
+
+for line in lines:
+    digits = []
+    for i, c in enumerate(line):
+        if c.isdigit():
+            digits.append(c)
+        for word in digit_dict:
+            if line[i:].startswith(word):
+                digits.append(str(digit_dict[word]))
+    concat = int(digits[0] + digits[-1])
+    total += concat
+
+print(total)
